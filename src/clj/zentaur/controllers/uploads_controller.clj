4f3c25e9bbe304d79c194a1682ad0622aa4f6e5e
aarkerio <mmontoya@gmail.com>
2018-06-14 18:18:06 -0500

    Uploads controller

--- src/clj/zentaur/controllers/uploads_controller.clj
+++ src/clj/zentaur/controllers/uploads_controller.clj
@@ -1,23 +1,25 @@
 (ns zentaur.controllers.uploads-controller
-  (:require [zentaur.models.uploads :as moduploads]
+  (:require [zentaur.models.uploads :as model-upload]
             [zentaur.controllers.base-controller :as basec]
             [zentaur.hiccup_templating.layout-view :as layout]
-            [zentaur.hiccup_templating.admin.uploads-view :as uploads-view]
+            [zentaur.hiccup_templating.admin.uploads-view :as admin-uploads-view]
             [clojure.tools.logging :as log]
             [ring.util.http-response :as response]))
 
+
+;;;;;;;;;;;;;;;;;;;;;;;     ADMIN FUNCTIONS    ;;;;;;;;;;;;;;;;;;;;
 ;; GET /admin/uploads
 (defn admin-uploads [request]
-  (let [base     (basec/set-vars request)
-        user-id  (-> request :identity :id)
-        _        (log/info (str ">>> user-id >>>>> " user-id))
-        files    (moduploads/get-uploads user-id)]
-    (layout/application (merge base {:title "Posts" :contents (uploads-view/index files) }))))
+  (let [base       (basec/set-vars request)
+        user-id    (-> request :identity :id)
+        csrf-field (:csrf-field base)
+        files      (model-upload/get-uploads user-id)]
+    (layout/application (merge base {:title "Posts" :contents (admin-uploads-view/index files csrf-field) }))))
 
 ;; POST /admin/uploads
 (defn upload-file [params identity]
   (let [user-id   (:id identity)]
     ;; (log/info (str ">>> FILE >>>>> " params " root-path >>> " root-path " user-file >>> " (str rand5 "-" (:filename user-file))))
-    (moduploads/save-upload! params identity)
+    (model-upload/save-upload! params identity)
     (-> (response/found "/admin/uploads"))))
 
--- src/clj/zentaur/hiccup_templating/admin/uploads_view.clj
+++ src/clj/zentaur/hiccup_templating/admin/uploads_view.clj
@@ -11,13 +11,13 @@
         [:div (:tags file)]
         [:div [:a {:href "/admin/uploads/delete/{{(:id file)}}"} "Delete"]]])
 
-(defn index [files content]
+(defn index [files csrf-field]
   (let [formatted-files (doall (for [file files]
                                  (formatted-file file)))]
   [:div {:class "row"}
     [:div {:class "span12"}
        (f/form-to [:post "/admin/uploads" {:enctype "multipart/form-data" :class "form-inline my-2 my-lg-0"}]
-           (f/hidden-field { :value (:csrf-field content)} "__anti-forgery-token")
+           (f/hidden-field { :value (:csrf-field csrf-field)} "__anti-forgery-token")
            (f/file-upload { :class "form-control mr-sm-2" :placeholder "file" } :userfile )
            (f/text-field  { :class "form-control mr-sm-2" :placeholder "email" } :email)
            (f/submit-button {:class "btn btn-outline-success my-2 my-sm-0" :name "submit"} "Anmeldung"))]]))
