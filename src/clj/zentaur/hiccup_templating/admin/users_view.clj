5b42e043ea5b403525e4098bda0d9ec57c0c1159
aarkerio <mmontoya@gmail.com>
2018-08-10 19:48:52 -0500

    updating Posts model

--- src/clj/zentaur/hiccup_templating/admin/users_view.clj
+++ src/clj/zentaur/hiccup_templating/admin/users_view.clj
@@ -4,14 +4,7 @@
             [clojure.tools.logging :as log]
             [hiccup.element :only (link-to)]))
 
-(defn formatted-user [user]
-  (let [fname      (:fname user)
-        lname      (:lname user)
-        uname      (:uname user)
-        email      (:email user)
-        active     (:active user)
-        id         (:id user)
-        created_at (:created_at user)]
+(defn formatted-user [{:keys [fname lname uname email active id created_at]}]
     [:tr
      [:td [:a {:href (str "/admin/users/edit/" id)} "Edit"]]
      [:td fname]
@@ -20,23 +13,23 @@
      [:td email]
      [:td created_at]
      [:td [:a {:href (str "/admin/users/active/" id "/" active)}  "Active"]]
-     [:td [:a {:href (str "/admin/users/delete/" id)}  "Delete"]]]))
+     [:td [:a {:href (str "/admin/users/delete/" id)}  "Delete"]]])
 
 (defn index [base users]
   (let [formatted-users (for [user users]
                           (formatted-user user))]
-  [:h1 {:id "root-app"} "Users and admins"]
-  [:div "Identity :  {{ (:identity content) }}"
-    [:div {:class "row"}
-      [:div {:class "span12" :id ""}
-         (f/form-to [:post "/admin/users/create" {:class "form-inline my-2 my-lg-0"}]
+    [:div {:class "content"}
+      [:h1 {:id "root-app"} "Users and admins"]
+      [:div {:id "image"} [:a {:href "#image"} [:img {:src "/img/icon_add.png" :alt "Add" :title "Add"}]]]
+      [:div {:class "row"}
+        [:div {:class "hidden-div" :id "divhide"}
+          (f/form-to [:post "/admin/users/create" {:class "form-inline my-2 my-lg-0"}]
             (f/hidden-field { :value (:csrf-field base)} "__anti-forgery-token")
             (f/text-field { :class "form-control mr-sm-2" :placeholder "First name" } :fname)
             (f/text-field { :class "form-control mr-sm-2" :placeholder "Last name" } :lname)
             (f/text-field { :class "form-control mr-sm-2" :placeholder "Username" } :uname)
             (f/text-field  { :class "form-control mr-sm-2" :placeholder "email" } :email)
-            (f/text-field  { :class "form-control mr-sm-2" :placeholder "email" } :prepassword)
-            (f/text-field  { :class "form-control mr-sm-2" :placeholder "email" } :prepassword2)
+            (f/text-field  { :class "form-control mr-sm-2" :placeholder "password" } :password)
             (f/submit-button {:class "btn btn-outline-success my-2 my-sm-0" :name "submit"} "Einrechen"))]]
       [:table {:class "some-classs"}
         [:thead
