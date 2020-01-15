7f114c2d9765f61e271707ad123138c86d920953
aarkerio <mmontoya@gmail.com>
2020-01-14 22:21:03 -0600

    Fixing sql

--- resources/migrations/20170826130418-add-users-table.up.sql
+++ resources/migrations/20170826130418-add-users-table.up.sql
@@ -31,10 +31,8 @@ INSERT INTO users (fname,
           4,
           true,
           true,
-          'bcrypt+sha512$31663163343233343262646331656136$12$472ab1393ef857b0b30769ebe654e627c0b0a4a3847a6f4e');  -- string: "password"
-
+          'bcrypt+sha512$31663163343233343262646331656136$12$472ab1393ef857b0b30769ebe654e627c0b0a4a3847a6f4e');
 --;;
-
 CREATE OR REPLACE FUNCTION trigger_set_timestamp()
 RETURNS TRIGGER AS $$
 BEGIN
