e4a13ff438f13a29abeeca19a2b881a4030cd6fd
aarkerio <mmontoya@gmail.com>
2020-01-26 17:21:35 -0600

    New PDF templates

--- src/clj/zentaur/controllers/export_controller.clj
+++ src/clj/zentaur/controllers/export_controller.clj
@@ -1,19 +1,19 @@
 (ns zentaur.controllers.export-controller
-  (:require [clojure.tools.logging :as log]
+  (:require [clojure.java.io :as io]
+            [clojure.tools.logging :as log]
             [zentaur.models.exports :as model-export]
             [ring.util.http-response :as response]))
 
-
 (defn export-test-pdf
   "GET /vclass/tests/exporttestpdf/:id. Create PDF."
-  [{:keys [params identity]}]
-  (let [test-id  (:id params)
+  [{:keys [path-params identity]}]
+  (log/info (str ">>> PATH PARAM >>>>> " path-params   " >>>> identity >>>"  identity ))
+  (let [test-id  (:id (update path-params :id #(Integer/parseInt %)))
         user-id  (:id identity)
-        pdf-path (model-export/export-pdf test-id user-id)]
-        file     (slurp pdf-path)
-    (log/info (str ">>> PARAM DOC PDF >>>>> " file))
-    (response/content-type (response/ok file) "application/pdf")
-     ))
+        pdf-path (model-export/export-pdf test-id user-id)
+        file     (io/input-stream pdf-path)]
+    (log/info (str ">>> PARAM DOC PDF >>>>> " pdf-path))
+    (response/content-type (response/ok file) "application/pdf")))
 
 (defn export-test-odf
   "GET /vclass/tests/exporttestodf/:id. Create ODF."
--- src/clj/zentaur/models/exports.clj
+++ src/clj/zentaur/models/exports.clj
@@ -5,20 +5,47 @@
             [zentaur.db.core :as db]
             [zentaur.libs.helpers :as h]))
 
-(defn export-pdf [test-id user-id]
-  (let [test (db/get-one-test {:id test-id :user-id user-id})
-        file-name "resources/public/tmp/document-perro-sfwewr.pdf"]
+(def questions-template
+  (pdf/template
+    [:paragraph
+     [:heading $question]
+     [:chunk {:style :bold} "questions: "] $occupation "\n"
+     [:chunk {:style :bold} "answers: "] $place "\n"
+     [:chunk {:style :bold} "country: "] $country
+     [:spacer]]))
+
+(def answers-template
+  (pdf/template
+    [:paragraph
+     [:heading $question]
+     [:chunk {:style :bold} "questions: "] $occupation "\n"
+     [:chunk {:style :bold} "answers: "] $place "\n"
+     [:chunk {:style :bold} "country: "] $country
+     [:spacer]]))
+
+(defn to-pdf [file-name test]
+  (let [subject (:subject test)
+        title   (:title test)]
     (pdf/pdf
-     [{}
+     [{:title title }
       [:list {:roman true}
-       [:chunk {:style :bold} "a bold item"]
-       "another item"
+       [:chunk {:style :bold} "Subject: " ]
+       "another item "  subject
        "yet another item"]
-      [:phrase "some text"]
-      [:phrase "some more text"]
+      [:phrase "some text lo que sea PEDAZO DE PENDEJO"]
+      [:phrase "some more text África {ñóñá}"]
       [:paragraph "yet more text"]]
-     file-name)
-    ))
+     file-name)))
+
+(defn export-pdf [test-id user-id]
+  (let [test      (db/get-one-test {:id test-id :user-id user-id})
+        _         (log/info (str ">>>  TEST >>>>> " test))
+        rand7     (crypto.random/hex 7)
+        title     (clojure.string/replace (:title test) #" " "_")
+        ;; final-pdf (questions-template questions)
+        file-name (str "resources/public/tmp/" title  "-" rand7 ".pdf")
+        _         (to-pdf file-name test)]
+    file-name))
 
 ;; (defn export-odf
 ;;   "Export to open document format"
