c1fe63753c1d79476c449da0c05dbb196c933b00
aarkerio <mmontoya@gmail.com>
2020-02-28 23:06:02 -0600

    Tests

--- test/clj/zentaur/models/tests_test.clj
+++ test/clj/zentaur/models/tests_test.clj
@@ -4,10 +4,21 @@
             [clojure.tools.logging :as log]
             [zentaur.models.tests :as mt]))
 
-;; (deftest ^:business-logic test-function
-;;   (testing "sub and string"
-;;     (is (= true  (.contains "The Band Named Isis" "Isis")))))
+(deftest ^:business-logic test-function
+   (testing "sub and string"
+     (is (= true  (.contains "The Band Named Isis" "Isis")))))
 
-;; (deftest ^:business-logic eg-tests (is (= 1 1)))
+(deftest ^:business-logic create-test!
+  (testing "Create a new test"
+    (let [user-id 1
+          title " Test title"
+          params  {:title title :hint "Some hint" :tags "tags" :user_id user-id :subject_id 1}
+          test    (mt/create-test! params user-id)]
+      (is (not (nil? (:tags test))))
+      (is (= title  (:title test)))))
+
+
+
+(deftest ^:business-logic eg-tests (is (= 1 1)))
 
 ;; (run-tests)
